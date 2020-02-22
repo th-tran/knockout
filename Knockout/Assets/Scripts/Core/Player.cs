6b618a3acac550a1c28c733ae817d3be901d64fd
Thomas Tran <tran.th.thomas@gmail.com>
2020-02-22 18:27:10 -0500

    Create player/enemy animations via spine, set up player for testing

---
+++ Knockout/Assets/Scripts/Core/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : PhysicsObject
+{
+    static Player instance;
+
+    public static Player Instance
+    {
+        get
+        {
+            if (instance == null) instance = GameObject.FindObjectOfType<Player>();
+            return instance;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
