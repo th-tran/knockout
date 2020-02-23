2b9c5e303b3c0be27327d059e70196584b5b2d4e
Thomas Tran <tran.th.thomas@gmail.com>
2020-02-22 19:10:12 -0500

    Set up player animation states

--- Knockout/Assets/Scripts/Core/Player.cs
+++ Knockout/Assets/Scripts/Core/Player.cs
@@ -4,6 +4,20 @@ using UnityEngine;
 
 public class Player : PhysicsObject
 {
+    Vector3 origLocalScale;
+    public bool frozen = false;
+    float launch = 1;
+    [SerializeField] float launchRecovery;
+    public float maxSpeed = 7;
+    public float jumpTakeOffSpeed = 7;
+    public bool recovering;
+    public float recoveryCounter;
+    public float recoveryTime = 2;
+    [SerializeField] GameObject graphic;
+    [SerializeField] Animator animator;
+
+    public RaycastHit2D ground;
+
     static Player instance;
 
     public static Player Instance
@@ -21,9 +35,61 @@ public class Player : PhysicsObject
 
     }
 
-    // Update is called once per frame
-    void Update()
+    protected override void ComputeVelocity()
     {
+        Vector2 move = Vector2.zero;
 
+        ground = Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y), -Vector2.up);
+        launch += (0 - launch) * Time.deltaTime * launchRecovery;
+
+        if (!frozen)
+        {
+            move.x = Input.GetAxis("Horizontal") + launch;
+
+            if (Input.GetButtonDown("Jump") && grounded)
+            {
+                velocity.y = jumpTakeOffSpeed;
+                //PlayJumpSound();
+                //PlayStepSound();
+            }
+
+            if (move.x > 0.01f)
+            {
+                if (graphic.transform.localScale.x < 0)
+                {
+                    graphic.transform.localScale = new Vector3(origLocalScale.x, transform.localScale.y, transform.localScale.z);
+                }
+            }
+            else if (move.x < -0.01f)
+            {
+                if (graphic.transform.localScale.x > 0)
+                {
+                    graphic.transform.localScale = new Vector3(-origLocalScale.x, transform.localScale.y, transform.localScale.z);
+                }
+            }
+
+            if (Input.GetMouseButtonDown (0))
+            {
+                animator.SetTrigger("attack");
+            }
+        }
+        else
+        {
+            launch = 0;
+        }
+
+        if (recovering)
+        {
+            recoveryCounter += Time.deltaTime;
+            if (recoveryCounter >= recoveryTime)
+            {
+                recoveryCounter = 0;
+                recovering = false;
+            }
+        }
+
+        animator.SetBool("grounded", grounded);
+        animator.SetFloat("velocityX", Mathf.Abs (velocity.x) / maxSpeed);
+        targetVelocity = move * maxSpeed;
     }
 }
